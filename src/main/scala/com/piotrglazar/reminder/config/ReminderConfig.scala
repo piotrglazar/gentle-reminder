9d5045ec482f82eb64177126e52b60b6e7aa9a50
Piotr Glazar <piotr.glazar@gmail.com>
2018-08-24 15:04:46 +0200

    User metiones in messages

--- src/main/scala/com/piotrglazar/reminder/config/ReminderConfig.scala
+++ src/main/scala/com/piotrglazar/reminder/config/ReminderConfig.scala
@@ -1,7 +1,6 @@
 package com.piotrglazar.reminder.config
 
-import com.piotrglazar.reminder.config.ReminderConfig.{JobConfig, RunConfig, SlackConfig}
-import com.piotrglazar.reminder.service.MessageSink.SinkName
+import com.piotrglazar.reminder.config.ReminderConfig.{JobConfig, RunConfig, SlackConfig, User}
 import com.typesafe.config.ConfigFactory
 import com.typesafe.scalalogging.LazyLogging
 import configs.{Configs, Result}
@@ -10,6 +9,7 @@ object ReminderConfig extends LazyLogging {
   case class RunConfig(host: String, port: Int)
   case class SlackConfig(token: String, channelId: String)
   case class JobConfig(name: String, sink: String, message: String)
+  case class User(name: String, slackName: String)
 
   def read(): Result[ReminderConfig] = {
     val config = ConfigFactory.load()
@@ -18,10 +18,11 @@ object ReminderConfig extends LazyLogging {
       runConfig <- Configs[RunConfig].get(config, "app")
       slackConfig <- Configs[SlackConfig].get(config, "slack")
       jobs <- Configs[List[JobConfig]].get(config, "jobs")
+      users <- Configs[List[User]].get(config, "users")
     } yield {
-      new ReminderConfig(runConfig, slackConfig, jobs)
+      ReminderConfig(runConfig, slackConfig, jobs, users)
     }
   }
 }
 
-case class ReminderConfig(runConfig: RunConfig, slackConfig: SlackConfig, jobs: List[JobConfig])
+case class ReminderConfig(runConfig: RunConfig, slackConfig: SlackConfig, jobs: List[JobConfig], users: List[User])
