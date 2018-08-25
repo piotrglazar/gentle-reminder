67f69e0299c93b95274a8364157e2d0dbe72a552
Piotr Glazar <piotr.glazar@gmail.com>
2018-08-25 16:46:58 +0200

    Slack sink update

--- src/main/scala/com/piotrglazar/reminder/service/SlackMessageSink.scala
+++ src/main/scala/com/piotrglazar/reminder/service/SlackMessageSink.scala
@@ -4,7 +4,13 @@ import scala.concurrent.Future
 class SlackMessageSink(private val slackClient: SlackClient) extends MessageSink {
 
   override def sendMessage(message: String, users: List[String]): Future[Unit] = {
-    slackClient.sendMessage(message)
+    slackClient.sendMessage(buildUserMentions(users) + message)
+  }
+
+  private def buildUserMentions(users: List[String]): String = {
+    users.foldLeft("") { case (acc, i) =>
+      acc +  s"<@$i> "
+    }
   }
 
   override def name: MessageSink.SinkName = MessageSink.SinkName("slack")
