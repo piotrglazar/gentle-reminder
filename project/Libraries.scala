960cda12efe7050528c714c3a6af0c43a1efea15
piotrglazar <piotr.glazar@gmail.com>
2019-06-26 15:41:08 +0200

    Sbt and library updates (#12)
    Sbt and library updates

--- project/Libraries.scala
+++ project/Libraries.scala
@@ -3,31 +3,30 @@ import sbt._
 object Libraries {
 
   object Core {
-    private val akkaVersion = "2.5.13"
-    private val akkaStreamsVersion = "10.1.3"
+    private val akkaVersion = "2.5.23"
 
     lazy val akka: Seq[ModuleID] = Seq(
       "com.typesafe.akka" %% "akka-actor" % akkaVersion,
       "com.typesafe.akka" %% "akka-stream" % akkaVersion,
-      "com.typesafe.akka" %% "akka-http" % akkaStreamsVersion
+      "com.typesafe.akka" %% "akka-http" % akkaVersion
     )
 
     lazy val akkaTest: Seq[ModuleID] = Seq(
       "com.typesafe.akka" %% "akka-testkit" % akkaVersion % Test,
       "com.typesafe.akka" %% "akka-stream-testkit" % akkaVersion % Test,
-      "com.typesafe.akka" %% "akka-http-testkit" % akkaStreamsVersion % Test
+      "com.typesafe.akka" %% "akka-http-testkit" % "10.1.8" % Test
     )
   }
 
   object Utils {
-    private val circeVersion = "0.10.0"
+    private val circeVersion = "0.11.1"
     
-    lazy val guava: ModuleID = "com.google.guava" % "guava" % "27.0.1-jre"
+    lazy val guava: ModuleID = "com.google.guava" % "guava" % "28.0-jre"
     lazy val logback: ModuleID = "ch.qos.logback" % "logback-classic" % "1.3.0-alpha4"
-    lazy val config: ModuleID = "com.typesafe" % "config" % "1.3.1"
+    lazy val config: ModuleID = "com.typesafe" % "config" % "1.3.4"
     lazy val pimpedConfig: ModuleID = "com.github.kxbmap" %% "configs" % "0.4.4"
-    lazy val logging: ModuleID = "com.typesafe.scala-logging" %% "scala-logging" % "3.9.0"
-    lazy val scheduling: ModuleID = "com.enragedginger" %% "akka-quartz-scheduler" % "1.6.1-akka-2.5.x"
+    lazy val logging: ModuleID = "com.typesafe.scala-logging" %% "scala-logging" % "3.9.2"
+    lazy val scheduling: ModuleID = "com.enragedginger" %% "akka-quartz-scheduler" % "1.8.0-akka-2.5.x"
     lazy val slackClient: ModuleID = "com.github.gilbertw1" %% "slack-scala-client" % "0.2.3"
     lazy val circe: Seq[ModuleID] = Seq(
       "io.circe" %% "circe-core",
@@ -38,8 +37,8 @@ object Libraries {
   }
 
   object TestUtils {
-    lazy val scalatest: ModuleID = "org.scalatest" %% "scalatest" % "3.0.5" % Test
-    lazy val mockito: ModuleID = "org.mockito" % "mockito-core" % "2.19.0" % Test
+    lazy val scalatest: ModuleID = "org.scalatest" %% "scalatest" % "3.0.8" % Test
+    lazy val mockito: ModuleID = "org.mockito" % "mockito-core" % "2.28.2" % Test
     lazy val restito: ModuleID = "com.xebialabs.restito" % "restito" % "0.9.3" % Test
   }
 
